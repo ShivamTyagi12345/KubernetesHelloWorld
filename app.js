e956bb17bb2b0254b1e442dde87b986c3bd1d355
ShivamTyagi12345 <mergesuccessful@gmail.com>
2022-11-17 19:00:26 +0530

    Starting from Express app again

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-
-const express = require('express')
-const app = express()
-
-const PORT =  3000
-const HOST = '0.0.0.0'
+//First, create a new directory where all the files would live. In this directory create a package.json file that describes your app and its dependencies:
 
 
- // Syntax is app.get(route , callback )
- // callback:They can be a middleware or an array of them
-app.get('/dukaan',(req,res)=>{
-    res.send('Hello World')
-})
+'use strict';
 
-app.listen(PORT,HOST,(err)=>{
-    if(err)
-        console.log(err)
-    console.log(`App is running on ${HOST} and port ${PORT}`)
+const express = require('express');
 
-})
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/dukaan', (req, res) => {
+  res.send('Dukaan is Open for Business 🍕');
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
